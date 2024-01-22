d683ed102848f07b9238b33b71c1a03f3b46acd2
ebanchik <ebanchik@gmail.com>
2024-01-21 20:22:18 -0500

    init + a bunch of stuff I should have committed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import { Button } from "./components/Button";
 
 function App() {
+  const items = ["New York", "San Francisco", "Tokyo", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>Hello World</Alert>
+      <Button>My Button</Button>
     </div>
   );
 }
